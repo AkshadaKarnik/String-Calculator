ad6d8b089e9b77620d040aa0f77463c2139b84e8
akshada <karnikakshada@gmail.com>
2025-09-05 17:31:19 +0530

    feat: handle new lines between numbers alongside commas (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,10 +3,8 @@
 class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.nil? || string_numbers == ""
-    # return string_numbers.to_i if string_numbers !~ /,|\n/
-    # 0
-    parts = string_numbers.split(",")
-    return parts.first.to_i if parts.size == 1
-    parts.map!(&:to_i).sum
+    # handles new lines between numbers (alongside commas)
+    parts = string_numbers.split(/,|\n/)
+    parts.map(&:to_i).sum
   end
 end
