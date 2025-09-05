23d6d39fca66e4b72a4945d4e53ca0d6fb23acd9
akshada <karnikakshada@gmail.com>
2025-09-05 17:12:12 +0530

    feat: sum two comma-separated numbers (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,10 @@
 class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.nil? || string_numbers == ""
-    return string_numbers.to_i if string_numbers !~ /,|\n/
-    0
+    # return string_numbers.to_i if string_numbers !~ /,|\n/
+    # 0
+    parts = string_numbers.split(",")
+    return parts.first.to_i if parts.size == 1
+    parts.map!(&:to_i).sum
   end
 end
