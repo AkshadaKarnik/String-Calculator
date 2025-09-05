ad9f098630f580fe63f491c33afdc991a2defc2d
akshada <karnikakshada@gmail.com>
2025-09-05 17:51:11 +0530

    feat: parse custom delimiter header (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,15 @@
 class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.nil? || string_numbers == ""
-    # handles new lines between numbers (alongside commas)
-    parts = string_numbers.split(/,|\n/)
+
+    delimiter_regex = /,|\n/
+    if string_numbers.start_with?("//")
+      header, string_numbers = string_numbers.split("\n", 2)
+      custom = header[2..] # everything after "//"
+      delimiter_regex = Regexp.union(Regexp.new(Regexp.escape(custom)), /\n/)
+    end
+
+    parts = string_numbers.split(delimiter_regex)
     parts.map(&:to_i).sum
   end
 end
