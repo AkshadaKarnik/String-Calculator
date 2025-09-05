379872cd7450556acf19963b866d2ae4d7a4c0f6
akshada <karnikakshada@gmail.com>
2025-09-05 17:01:36 +0530

    feat: handle single number (green)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,5 +3,7 @@
 class StringCalculator
   def add(string_numbers)
     return 0 if string_numbers.nil? || string_numbers == ""
+    return string_numbers.to_i if string_numbers !~ /,|\n/
+    0
   end
 end
